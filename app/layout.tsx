caa2168c0d2054f504bcf8136d258d5add5b59b3
Sanju Reddy <sanju@Sanju_Reddy.(none)>
2025-09-25 00:37:56 +0530

    last commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,19 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, IBM_Plex_Serif, Inter } from "next/font/google";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
-  variable: "--font-ibm-plex-serif",
-  subsets: ["latin"],
-  weight: ["400", "700"]
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
 
 export const metadata: Metadata = {
-  title: "Margin",
-  description: "Margin is a modern banking platform for everyone.",
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
@@ -34,11 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${ibmPlexSerif.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
     </html>
   );
 }
